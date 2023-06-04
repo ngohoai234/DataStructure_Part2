4ff4b1a1b735b4f44427d70525564378a9a6156c
ngohoai234 <cangod05@gmail.com>
2023-06-04 13:20:57 +0700

    Coding leet code max product

--- Array/maxProduct.js
+++ Array/maxProduct.js
@@ -1,3 +1,31 @@
-const maxProduct = (s) => {};
+const maxProduct = (s) => {
+  const n = s.length;
+  let res = 0;
+  const pali = {};
+  const length = 1 << n; // 2 ^ n
+
+  for (let mask = 1; mask < length; mask++) {
+    let subSequence = '';
+    for (let i = 0; i < n; i++) {
+      //   check whether mask includes in range
+      if (mask & (1 << i)) {
+        subSequence += s[i];
+      }
+    }
+    if (subSequence === subSequence.split('').reverse().join('')) {
+      pali[mask] = subSequence.length;
+    }
+  }
+
+  for (const m1 in pali) {
+    for (const m2 in pali) {
+      // disjoint
+      if ((m1 & m2) === 0) {
+        res = Math.max(res, pali[m1] * pali[m2]);
+      }
+    }
+  }
+  return res;
+};
 
 console.log(maxProduct('leetcodecom'));

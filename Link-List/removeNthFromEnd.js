2ebdeabdc645d66dd63013aebfe8098cda1afd42
ngohoai234 <cangod05@gmail.com>
2023-11-18 15:53:52 +0700

    removeNthFromEnd

---
+++ Link-List/removeNthFromEnd.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  const dummy = new ListNode();
+
+  dummy.next = head;
+
+  let l = dummy;
+  let r = head;
+
+  while (r && n > 0) {
+    n--;
+    r = r.next;
+  }
+
+  while (r) {
+    r = r.next;
+    l = l.next;
+  }
+
+  //   delete
+
+  l.next = l.next.next;
+
+  return dummy.next;
+};

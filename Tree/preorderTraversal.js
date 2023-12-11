491178839bced5a387a2bfe258aec5ab4e431352
ngohoai234 <cangod05@gmail.com>
2023-12-11 22:19:14 +0700

    preorderTraversal

---
+++ Tree/preorderTraversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  const res = [];
+  const stack = [root];
+
+  while (stack.length) {
+    const node = stack.pop();
+    if (node) {
+      res.push(node.val);
+      stack.push(node.right);
+      stack.push(node.left);
+    }
+  }
+
+  return res;
+};

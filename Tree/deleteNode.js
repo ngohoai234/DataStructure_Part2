b04964824730354d44c0e323e467f39e5d5504ad
ngohoai234 <cangod05@gmail.com>
2023-12-22 22:43:15 +0700

    deleteNode

---
+++ Tree/deleteNode.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} key
+ * @return {TreeNode}
+ */
+var deleteNode = function (root, key) {
+  if (!root) {
+    return null;
+  }
+
+  if (root.val > key) {
+    root.left = deleteNode(root.left, key);
+  } else if (root.val < key) {
+    root.right = deleteNode(root.right, key);
+  } else {
+    if (!root.left) {
+      return root.right;
+    } else if (!root.right) {
+      return root.left;
+    } else {
+      // find the node with the same value as the key
+      let curr = root.right;
+
+      while (curr.left) {
+        curr = curr.left;
+      }
+
+      //   swap the value
+      root.val = curr.val;
+
+      //   delete the right node
+      root.right = deleteNode(root.right, root.val);
+    }
+  }
+
+  return root;
+};
